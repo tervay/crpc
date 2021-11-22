631d2f3470a09e380ae529d4da8fe62b848359b3
Justin Tervay <justin@tervay.com>
2021-11-21 19:25:08 -0500

    wew

--- src/tba/tba.cpp
+++ src/tba/tba.cpp
@@ -1,8 +1,11 @@
 #include "cpr/cpr.h"
-#include "src/protos/tba.grpc.pb.h"
+#include "src/protos/tpa.grpc.pb.h"
+// #include "src/protos/tba.grpc.pb.h"
 
 namespace tba {
 namespace models {
-void foo(tpa::Team team) {}
+void foo(tpa::Team team) {
+    tpa::Team team;
+}
 }  // namespace models
 }  // namespace tba
